edfe4c70adee6ce23e550c99c6654e6803d3c7b8
groovychoons <zara@siddique.eu>
2024-12-31 17:28:46 +0000

    Maybe I've figured out how helmet works?

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import { useLocation } from "react-router-dom";
 import About from './pages/About';
 import CV from './pages/CV';
@@ -22,27 +22,47 @@ function App() {
   return (
     <div className="App">
       <HelmetProvider>
-          <Container className="pt-5">
-            <Navbar expand="lg">
-              <Navbar.Brand href="/">Zara Siddique</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav>
-                  <Nav.Link href="/">About</Nav.Link>
-                  <Nav.Link href={cv} target="_blank" rel="noreferrer">CV</Nav.Link>
-                  <Nav.Link href="/blog">Blog</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-            <hr className='mb-5' />
+        <Helmet>
 
-            <Routes>
-              <Route path="/" element={<About />} />
-              <Route path="/cv" element={<CV />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:id" element={<BlogPost />} />
-            </Routes>
-          </Container>
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://zarasiddique.com/" />
+          <meta property="og:title" content="Zara Siddique" />
+          <meta property="og:description" content="A website all about Zara Siddique." />
+          <meta property="og:image"
+            content="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdXB3azYxNzAyNDQ2LXdpa2ltZWRpYS1pbWFnZS1rb3dia3MxZS5qcGc.jpg" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:url" content="https://zarasiddique.com/" />
+          <meta name="twitter:title" content="Zara Siddique" />
+          <meta name="twitter:description" content="A website all about Zara Siddique." />
+          <meta name="twitter:image"
+            content="https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdXB3azYxNzAyNDQ2LXdpa2ltZWRpYS1pbWFnZS1rb3dia3MxZS5qcGc.jpg" />
+
+          <meta name="title" content="Zara Siddique" />
+          <meta name="description" content="A website all about Zara Siddique." />
+          <meta name="author" content="Zara Siddique" />
+        </Helmet>
+        <Container className="pt-5">
+          <Navbar expand="lg">
+            <Navbar.Brand href="/">Zara Siddique</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <Nav.Link href="/">About</Nav.Link>
+                <Nav.Link href={cv} target="_blank" rel="noreferrer">CV</Nav.Link>
+                <Nav.Link href="/blog">Blog</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <hr className='mb-5' />
+
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/cv" element={<CV />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogPost />} />
+          </Routes>
+        </Container>
       </HelmetProvider>
     </div>
   );
