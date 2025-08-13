62f77a07c3d55ec723fc222be0e20a61e2cd734a
groovychoons <zara@siddique.eu>
2025-08-13 13:47:15 +0100

    Temporary blog remove

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -2,14 +2,14 @@ import EMNLP from "../posts/EMNLP";
 import ShiftingPerspectives from "../posts/ShiftingPerspectives";
 
 const blogPosts = [
-  {
-    id: 2,
-    title: "Shifting Perspectives Paper: Creating a dial to 'turn down' social bias in LLMs",
-    date: "2025/03/12",
-    slug: "shifting-perspectives",
-    excerpt: "The blog post for the Shifting Perspectives paper.",
-    component: ShiftingPerspectives
-  },
+  // {
+  //   id: 2,
+  //   title: "Shifting Perspectives Paper: Creating a dial to 'turn down' social bias in LLMs",
+  //   date: "2025/03/12",
+  //   slug: "shifting-perspectives",
+  //   excerpt: "The blog post for the Shifting Perspectives paper.",
+  //   component: ShiftingPerspectives
+  // },
   {
     id: 1,
     title: "My Favourite EMNLP 2024 Papers",
